8f8c972cebd05d7344e096c432828ed753392079
massoncorlette <massoncorlette07@gmail.com>
2025-03-06 05:27:33 -0500

    error: a lot to process with all the new modules and concepts, can't seem to pass in the path without having .html included in path name?

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ var fs = require('fs');
 http.createServer(function(req,res) { // creating basic server client side
   let q = url.parse(req.url, true); // use parse to read parts of URL
 
-  let filename = "." + (q.pathname = '/') ? "./index.html" : q.pathname;
+  let filename = (q.pathname === '/') ? "./index.html" : "." + q.pathname;
 
   console.log(filename);
+  console.log(q.pathname);
+
+   fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
 
 }).listen(8080)
