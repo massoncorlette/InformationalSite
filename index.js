ceda985693163b207ce7ef5458640425fc1f23c4
massoncorlette <massoncorlette07@gmail.com>
2025-03-06 02:44:37 -0500

    using a ternary to pass index html path by default if no pathname

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ let url = require('url');
 var fs = require('fs');
 
 
-http.createServer(function(req,res) {
-  let q = url.parse(req.url, true);
-  let filename = "." + q.pathname;
+http.createServer(function(req,res) { // creating basic server client side
+  let q = url.parse(req.url, true); // use parse to read parts of URL
 
+  let filename = "." + (q.pathname = '/') ? "./index.html" : q.pathname;
 
-})
+  console.log(filename);
+
+}).listen(8080)
