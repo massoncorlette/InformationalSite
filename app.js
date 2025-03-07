fbd869d390b8d15c58decf1d48e2d822970e196a
massoncorlette <massoncorlette07@gmail.com>
2025-03-07 03:15:45 -0500

    I have absolutely no idea what I am doing

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.sendFile('Repos/InformationalSite/index.html'));
+app.get("/", function (req, res, next) {
+  var options = {
+    root: path.join(__dirname, 'public'),
+    
+  }
+
+  if (!(req.params.name)) {
+    res.sendFile('index.html');
+  }
+
+  let fileName = req.params.name
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
