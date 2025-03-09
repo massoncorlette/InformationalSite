66b67f0e9180dd809034bc830a048e64ebdb5bc8
massoncorlette <massoncorlette07@gmail.com>
2025-03-08 23:19:27 -0500

    added a initial app.get for just the index path, catching the request first before the dynamic app.get path for loading in the other pages

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// initial index get
+app.get('/', function (req, res, next) {
+  let options = {
+    root:path.join(__dirname,),
+
+  }
+  let fileName = "index.html";
+
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  });
+});
+
 app.get('/:name', function (req, res, next) {
   let options = {
-    root: path.join(__dirname, ),
+    root: path.join(__dirname,),
     
   }
   let fileName = req.params.name + ".html";
@@ -15,8 +32,8 @@ app.get('/:name', function (req, res, next) {
     } else {
       console.log('Sent:', fileName)
     }
-  })
-})
+  });
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
